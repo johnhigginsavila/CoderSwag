06d8a2a8366e5e2754c15c4b59f0e09b22a7942b
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-10-26 04:45:29 +0800

    Lesson 6

--- app/src/main/java/com/johnhigginsmavila/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/johnhigginsmavila/coderswag/Controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.johnhigginsmavila.coderswag.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -7,6 +8,7 @@ import com.johnhigginsmavila.coderswag.Adapters.CategoryAdapter
 import com.johnhigginsmavila.coderswag.Adapters.CategoryRecycleAdapter
 import com.johnhigginsmavila.coderswag.R
 import com.johnhigginsmavila.coderswag.Services.DataService
+import com.johnhigginsmavila.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryRecycleAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories) { category ->
+            val productIntent = Intent(this, ProductsActivity::class.java)
+
+            productIntent.putExtra(EXTRA_CATEGORY, category.title)
+            startActivity(productIntent)
+        }
 
         categoryListView.adapter = adapter
 
